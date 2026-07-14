729c59d33c375d6632e626a22d5ef00515dc3e6b
zakaria-moussaoui <zakariamoussaoui370@gmail.com>
2026-07-14 09:17:20 +0100

    Config Railway : Dockerfile, port, connexion, migrations auto

--- g2soire/Program.cs
+++ g2soire/Program.cs
@@ -4,20 +4,23 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// --- Port Railway (utilise le port imposé par Railway, sinon 8080 en local) ---
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+builder.WebHost.UseUrls($"http://*:{port}");
+
+// --- Chaîne de connexion : Railway en ligne, sinon appsettings en local ---
+var railwayUrl = Environment.GetEnvironmentVariable("MYSQL_URL");
+var cnx = !string.IsNullOrEmpty(railwayUrl)
+    ? ConvertMysqlUrl(railwayUrl)
+    : builder.Configuration.GetConnectionString("cnx");
 
 builder.Services.AddDbContext<AppContex>(options =>
-    options.UseMySql(
-        builder.Configuration.GetConnectionString("cnx"),
-        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("cnx"))
-    )
-);
+    options.UseMySql(cnx, ServerVersion.AutoDetect(cnx)));
 
 builder.Services.AddScoped<IDao, DaoImpl>();
 builder.Services.AddScoped<IServices, VService>();
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -25,19 +28,29 @@ builder.Services.AddHttpClient();
 
 var app = builder.Build();
 
-app.UseStaticFiles();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+// --- Crée les tables dans la base Railway automatiquement au démarrage ---
+using (var scope = app.Services.CreateScope())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    var db = scope.ServiceProvider.GetRequiredService<AppContex>();
+    db.Database.Migrate();
 }
 
-app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+// Swagger activé tout le temps (pratique pour tester en ligne)
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseAuthorization();
 
 app.MapControllers();
 
 app.Run();
+
+// --- Convertit l'URL Railway (mysql://user:pass@host:port/db) en chaîne EF Core ---
+static string ConvertMysqlUrl(string url)
+{
+    var uri = new Uri(url);
+    var userInfo = uri.UserInfo.Split(':');
+    return $"server={uri.Host};port={uri.Port};database={uri.AbsolutePath.TrimStart('/')};user={userInfo[0]};password={Uri.UnescapeDataString(userInfo[1])}";
+}
